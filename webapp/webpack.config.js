d0bc88cb4dad103548f080a8954ff5781d2a90e3
root <root@ip-172-31-38-224.ap-southeast-7.compute.internal>
2026-02-11 04:45:04 +0700

    fix(build): exclude test files from webpack build

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: /node_modules/,
+                exclude: [/node_modules/, /\.test\.tsx?$/, /test\//],
             },
             {
                 test: /\.css$/,
